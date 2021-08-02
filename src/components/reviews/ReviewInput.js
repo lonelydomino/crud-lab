e3c12b10869d250085631b20958205a1aa6a2d30
Michael Martinez <zeroeclipse@live.com>
2021-08-01 19:07:40 -0700

    commit tree 4fc5b9f6a4a630a901821341d644f4b4b7d983d2

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  state = {
+    text: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId})
+
+  }
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleSubmit}>
+          <label>Add Review: </label>
+          <input onChange={this.handleChange} type="text" name="review"></input>
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+  
   render() {
+    const associatedReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId);
     return (
       <ul>
-        Reviews
+        {associatedReviews.map((review, index) => {
+      return <Review key={index} review={review} deleteReview={this.props.deleteReview} />
+    })}
       </ul>
     );
   }
